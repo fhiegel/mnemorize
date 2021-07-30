8b4ee674c13ada5eb01d6e1933d4db6c61cc6021
Fabien HIEGEL <fabien.hiegel@gmail.com>
2021-07-30 17:14:01 +0200

    docs: add tabs on demo website

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import './App.css';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import FlipCards from "./components/flip-card-list/FlipCards";
+import FlipCard from "./components/flip-card/FlipCard";
+import './App.css';
 
 const questionsAndAnswers = [
   {
@@ -24,7 +27,26 @@ const questionsAndAnswers = [
 ];
 
 const App = () => <div className="App">
-  <FlipCards questionsAndAnswers={questionsAndAnswers}/>
+  <Tabs>
+    <TabList>
+      <Tab>Carte seule</Tab>
+      <Tab>Liste</Tab>
+      <Tab>Deck</Tab>
+    </TabList>
+
+    <TabPanel>
+      <FlipCard questionAndAnswer={{
+        question: { title: 'Comment se souvenir de tout pour quasiment toujours ?' },
+        answer: { title: 'Utiliser le mécanisme de la répétition espacée, et des flip-cards' },
+      }}/>
+    </TabPanel>
+    <TabPanel>
+      <FlipCards questionsAndAnswers={questionsAndAnswers}/>
+    </TabPanel>
+    <TabPanel>
+      <p>TODO : un deck n'est pas une liste</p>
+    </TabPanel>
+  </Tabs>
 </div>;
 
 export default App;
